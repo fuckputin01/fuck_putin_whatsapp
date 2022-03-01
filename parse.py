e85b53df2ab93d857862b6e8b2b6f11473309318
Fuck Putin <russianwarship@gofuckyourself.com>
2022-03-01 12:01:22 +0100

    Updated phones and text Putin army goes to hell

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 import re
-pattern = '[(]{0,1}[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}[(]{0,1}' # pattern for matching all ASCII characters, digits, and repetitions of
+pattern = '[(]{0,1}[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}[(]{0,1}'
+pattern2 = '[(]{0,1}[0-9]{3}[\s-]+[0-9]{3}[\s-]+[0-9]{2}[\s-]+[0-9]{2}[(]{0,1}'
 #                             # them (+)
 # lines = "property"           # adding input string, raw_input("Enter Combination: ")
 # ls = re.findall(pattern,lines)
@@ -8,8 +9,8 @@ pattern = '[(]{0,1}[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}[(]{0,1}' # pattern for ma
 def parse():
     with open('1.txt', 'r') as f:
         q = f.read()
-        ls = re.findall(pattern, q)
-        with open('list.csv', 'w') as w:
+        ls = re.findall(pattern2, q)
+        with open('list2.csv', 'w') as w:
             w.write('Primary Phone\n')
             for p in ls:
                 w.write(f'+7-{p}\n')
